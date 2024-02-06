085b0639963abb2101d8501d232d1d635d89cae1
Grace Colverd <gb669@cam.ac.uk>
2024-02-06 17:15:12 +0000

    push updates

--- run_whole_process.py
+++ run_whole_process.py
@@ -1,28 +1,19 @@
-# run_whole_process.py 
-
-# call a python script 
-# call match_postcode_to_uprns.py with kwargs gas+elec ='gas' 
-
-
-# call script gen_nrom_fuel.py with kwargs gas+elec ='gas' and filtered =True 
-
-# call script post_process_fuel.py with kwargs gas+elec ='gas' 
-
 import subprocess
 
+
+################################ Update the following variables ################################
 gas_elec = 'gas'
-filt=True 
-
 fuel_year = 2022
-onsud = '/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022/Data'
-raw_elec = f'/Volumes/T9/Data_downloads/UKGOV_Gas_elec/Postcode_level_all_meters_electricity_{fuel_year}.csv'
-raw_gas = f'/Volumes/T9/Data_downloads/UKGOV_Gas_elec/Postcode_level_gas_{fuel_year}.csv'
 
-pc_map ='/Volumes/T9/Data_downloads/Postcode_to_LSOA_mapping/PCD_OA21_LSOA21_MSOA21_LAD_AUG23_UK_LU 2.csv'
+################################ Update paths to downloaded data ################################ 
+onsud = '/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022/Data' #ONS ONSUD DATA
+raw_elec = f'/Volumes/T9/Data_downloads/UKGOV_Gas_elec/Postcode_level_all_meters_electricity_{fuel_year}.csv' #electricity by postcode 
+raw_gas = f'/Volumes/T9/Data_downloads/UKGOV_Gas_elec/Postcode_level_gas_{fuel_year}.csv' #gas by postcode 
 
-link = '/Volumes/T9/Data_downloads/Versik_building_data/UKBuildings_Edition_14_ABC_link_file.csv'
-lk = '/Volumes/T9/Data_downloads/Eng_wales_boundary_shapefiles/LSOA_(2021)_to_Built_Up_Area_to_Local_Authority_District_to_Region_(December_2022)_Lookup_in_England_and_Wales_v2.csv'
-
+pc_map ='/Volumes/T9/Data_downloads/Postcode_to_LSOA_mapping/PCD_OA21_LSOA21_MSOA21_LAD_AUG23_UK_LU 2.csv' #postcode to LSOA mapping
+link = '/Volumes/T9/Data_downloads/Versik_building_data/UKBuildings_Edition_14_ABC_link_file.csv' # Link file 
+# lk = '/Volumes/T9/Data_downloads/Eng_wales_boundary_shapefiles/LSOA_(2021)_to_Built_Up_Area_to_Local_Authority_District_to_Region_(December_2022)_Lookup_in_England_and_Wales_v2.csv'
+################################################################################################ 
 
 def run_script(script_name, **kwargs):
     # Construct the command with script name and kwargs
