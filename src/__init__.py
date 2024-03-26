cfd79d0d2216369db630420ae4303bf862edc7c9
Grace Colverd <gb669@cam.ac.uk>
2024-03-26 11:01:19 +0000

    add pre process verisk

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,6 @@ from .utils import *
 from .buildings import *
 from .postcode_attr import * 
 from .multi_thread import * 
-from .Post_attr_gas import * 
+from .Post_attr_gas_old import * 
 from .pre_process_buildings import *
 
