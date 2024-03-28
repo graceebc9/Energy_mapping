4a45c04e9f8af058566d9e02fa50c37c9a5181ae
Grace Colverd <gb669@cam.ac.uk>
2024-03-28 18:51:14 +0000

    add storage file

--- split_onsud_file.py
+++ split_onsud_file.py
@@ -25,13 +25,16 @@ def split_onsud_file(path_to_onsud_file, path_to_pcshp, output_directory, logfil
         os.makedirs(batch_dir, exist_ok=True)   
 
         batch_filename = os.path.join(batch_dir, f"batch_{i//batch_size}.txt" ) 
-        batch_list.append(batch_filename)
+
+        with open('batch_paths.txt', 'w') as f:
+                f.write(f"{batch_filename}\n")
+
         with open(batch_filename, 'w') as f:
             for pc in batchs:
                 f.write(f"{pc}\n")
 
     print('Batches saved')
-    return batch_list 
+   
 
 if __name__ == "__main__":
     print('Loading files')
@@ -51,8 +54,6 @@ if __name__ == "__main__":
 
 
 #     export DATA_DIR='/Users/gracecolverd/New_dataset/old/testing'
-
-
 # export ONSUD_PATH='/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022/Data/ONSUD_DEC_2022_EE.csv'
 # export PC_SHP_PATH='/Volumes/T9/Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998/codepoint-poly_5267291'
 
