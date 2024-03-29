e1b69194eff49d8ac35c54e34205fce4baa7601d
Grace Colverd <gb669@cam.ac.uk>
2024-03-29 10:30:11 +0000

    add auto extraction of onsud path from batch

--- run_fuel_calc_for_batch.py
+++ run_fuel_calc_for_batch.py
@@ -7,6 +7,12 @@ def load_ids_from_file(file_path):
         ids = file.read().splitlines()
     return ids
 
+def get_onsud_path(onsud_dir, label ):
+
+    # DATA_DIR='/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022'
+    date = onsud_dir.split('/')[-1].split('ONSUD_')[-1]
+    filepath = os.path.join(onsud_dir, f'Data/ONSUD_{date}_{label}.csv' ) 
+    return filepath
 
 def main( batch_path, data_dir, gas_path, elec_path, path_to_onsud_file, path_to_pcshp, INPUT_GPK  ):
     label = path_to_onsud_file.split('/')[-1].split('.')[0].split('_')[-1]
@@ -29,18 +35,20 @@ if __name__ == "__main__":
     data_dir = os.environ.get('DATA_DIR')
     gas_path = os.environ.get('GAS_PATH')
     elec_path = os.environ.get('ELEC_PATH')
-    path_to_onsud_file= os.environ.get('ONSUD_PATH')
+    onsud_dir= os.environ.get('ONSUD_DIR')
     path_to_pcshp= os.environ.get('PC_SHP_PATH')
     input_gpk_building = os.environ.get('BUILDING_PATH')
     batch_path = os.environ.get('BATCH_PATH') 
+    label = batch_path.split('/')[-2]
+    path_to_onsud_file = get_onsud_path( onsud_dir, label )
     
     print('starting main')
     main( batch_path, data_dir, gas_path, elec_path, path_to_onsud_file, path_to_pcshp, INPUT_GPK = input_gpk_building  ) 
 
 
-# export  BATCH_PATH='/Users/gracecolverd/New_dataset/test/batches/EE/batch_0.txt'
+# export  BATCH_PATH='/Users/gracecolverd/New_dataset/test/batches/NE/batch_0.txt'
 # export DATA_DIR='/Users/gracecolverd/New_dataset/test'
-# export ONSUD_PATH='/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022/Data/ONSUD_DEC_2022_EE.csv'
+# export ONSUD_DIR='/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022'
 # export PC_SHP_PATH='/Volumes/T9/Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998/codepoint-poly_5267291'
 # export GAS_PATH='/Volumes/T9/Data_downloads/UKGOV_Gas_elec/Postcode_level_gas_2022.csv'
 # export ELEC_PATH='/Volumes/T9/Data_downloads/UKGOV_Gas_elec/Postcode_level_all_meters_electricity_2022.csv'
