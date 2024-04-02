4d0ef5bf60638c3cce0036ae265c845fa6372ac3
Grace Colverd <gb669@cam.ac.uk>
2024-04-02 15:35:45 +0100

    add glob tabl

--- create_global_tables.py
+++ create_global_tables.py
@@ -12,16 +12,20 @@ from src.buildings import get_bounding_boxes , calculate_bounding_boxes, generat
 
 
 
-input_gpk = '/Volumes/T9/Data_downloads/Versik_building_data/2024_03_22_updated_data/UKBuildings_Edition_15_new_format_upn.gpkg' 
-output_path = '/Users/gracecolverd/New_dataset/test'
+# input_gpk = '/Volumes/T9/Data_downloads/Versik_building_data/2024_03_22_updated_data/UKBuildings_Edition_15_new_format_upn.gpkg' 
+# output_path = '/Users/gracecolverd/New_dataset/test'
 
 
 def main():
+    input_gpk = os.environ.get('BUILDING_PATH')
+    output_path = os.environ.get('OUTPUT_PATH')
+    
     ds = ogr.Open(input_gpk)
     layer = ds.GetLayer()
     extent = layer.GetExtent()
     bounding_boxes = calculate_bounding_boxes(extent, chunk_height=10000, chunk_width=10000)
     bbox_test = bounding_boxes[0:1000]
+    
     print('boxes to process: ', len(bounding_boxes  ))
     compute_global_modal_age(bbox_test, input_gpk, output_path) 
     print('Complete')
