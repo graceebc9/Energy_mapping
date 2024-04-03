23abf15860704cb75c32dfb9bfac7b045df144d4
Grace Colverd <gb669@cam.ac.uk>
2024-04-03 12:27:39 +0100

    new age calc

--- src/age_calc.py
+++ src/age_calc.py
@@ -20,7 +20,6 @@ def age_mapping():
     return category_to_ordinal, ordinal_to_category, category_order 
 
 
-
 def bucket_age(df):
     df['premise_age_bucketed'] = np.where(df['premise_age'].isin(['Pre 1837', '1837-1869', '1870-1918']), 'Pre 1919', df['premise_age'])
 
@@ -30,88 +29,70 @@ def bucket_age(df):
     
     return df 
 
-def calculate_modal_age_band(df ):
+def calculate_modal_age_band(df , age_col):
     """ Fn to calculate the modal age band of a column for a df (needs contain all same postcodes)
     Assumptions: less than 10% unknowns ignore, all varaibles pre 1919 to one variable 
     """
+    # df = df[df['premise_age_bucketed']!='Unknown date']  
+    mode = df[age_col].mode()[0]
 
-    df = df[df['premise_age_bucketed']!='Unknown date']  
-    mode = df['premise_age_bucketed'].mode()[0]
     return  mode 
     
-def calc_range_age(df):
+def calc_range_age(df, ord_col):
     """ Fn to calculate the range of the age band of a column for a df (needs contain all same postcodes)
     Assumptions: all varaibles pre 1919 to one variable 
     """
     # alll variables pre 1919 updated to be one variable 
 
-    min_age = df['ordinal_age'].min()
-    max_age = df['ordinal_age'].max()
-    distinct_ages = df['ordinal_age'].nunique()
+    min_age = df[ord_col].min()
+    max_age = df[ord_col].max()
+    distinct_ages = df[ord_col].nunique()
 
-    
     return min_age, max_age, distinct_ages 
 
-def calculate_median_age_band(df):
+def calculate_median_age_band(df, age_col, ord_col):
     """
     Calculate the median of a categorical column based on a specified ordering.
-
-    Parameters:
-    - df: pandas DataFrame containing the data.
-    - modal_ignore: str, whether to use the modal value for unknown dates ('modal') or ignore unknown dates ('ignore').
-
-    Returns:
-    - The category that represents the median.
     """
     def calc_med(df):
-        ordinal_values = df['premise_age_bucketed'].map(category_to_ordinal)
+        ordinal_values = df[age_col].map(category_to_ordinal)
         median_ordinal = ordinal_values.median()
         median_category = category_order[int(np.round(median_ordinal))]
         return median_category
 
     def calc_iqr(df):
-        q1 =  df['ordinal_age'].quantile(0.25)
-        q3 = df['ordinal_age'].quantile(0.75)
+        q1 =  df[ord_col].quantile(0.25)
+        q3 = df[ord_col].quantile(0.75)
         iqr = q3 - q1
         return iqr 
 
     category_to_ordinal, _ , category_order = age_mapping() 
-    df_ignore  = df[df['premise_age_bucketed']!='Unknown date'].copy() 
 
-    df['premise_age_bucketed'] = df['premise_age_bucketed'].replace('Unknown date', df['premise_age_bucketed'].mode()[0])    
+    iqr = calc_iqr(df)
+    median = calc_med(df)
 
-    if len(df_ignore) == 0:
-        print('No data for ignore')
-        ignore_median = np.nan 
-    else:
-        ignore_median = calc_med(df_ignore)
-
-    if len(df) == 0:
-        modal_median = np.nan
-    else:
-        modal_median = calc_med(df)
-
-    ignore_iqr = calc_iqr(df_ignore)
-    modal_iqr = calc_iqr(df)
+    return median, iqr 
 
 
-        
-
-    return ignore_median, modal_median, ignore_iqr, modal_iqr
-        
-
-
-def calc_age_attributes(df):
+def calc_age_attributes(df, prefix):
     """ Fn to calculate the median age band of a column for a df 
     Inputs:
     --df: dataframe containing only one postcode 
     """
     
-    ignore_median, modal_median,  ignore_iqr, modal_iqr = calculate_median_age_band(df )
-    modal_age  = calculate_modal_age_band(df)
 
-    min_age, max_age, distinct_ages   = calc_range_age(df)
+    if prefix =='localfill':
+        age_col = 'loc_fill_age'
+        ord_col = 'loc_ordinal'
+    elif prefix =='globalfill':    
+        age_col = 'global_fill_age'
+        ord_col = 'global_ordinal'
 
+    median,  iqr  = calculate_median_age_band(df , age_col, ord_col)
+    modal_age  = calculate_modal_age_band(df, age_col)
+
+    min_age, max_age, distinct_ages   = calc_range_age(df, age_col)
+    print(min_age)
     range_age = max_age - min_age
 
     _, ord_to_cat, _  = age_mapping() 
@@ -119,26 +100,86 @@ def calc_age_attributes(df):
     min_age = ord_to_cat[min_age  ]
     max_age = ord_to_cat[max_age]
     
+    return  median, modal_age , min_age, max_age, range_age,  distinct_ages,  iqr
+
+# def calc_age_attributes(df, global_df):
+#     """ Fn to calculate the median age band of a column for a df 
+#     Inputs:
+#     --df: dataframe containing only one postcode 
+#     """
+#     if len(df)==1:
+#         print('Only one building')
+#         age = df['premise_age'].values[0]
+#         if age =='Unknown date':
+#             if df['premise_type'].values[0] == None :
+#                 print('Unknown date and none type')
+#                 return np.nan, np.nan, age, age, age, 0, 1, np.nan, np.nan
+#             else: 
+#                 df.merge(global_df, on =['premise_use', 'premise_use'])
+#         return np.nan, np.nan, age, age, age, 0, 1, np.nan, np.nan
+    
+#     ignore_median, modal_median,  ignore_iqr, modal_iqr = calculate_median_age_band(df )
+#     modal_age  = calculate_modal_age_band(df)
+
+#     min_age, max_age, distinct_ages   = calc_range_age(df)
+
+#     range_age = max_age - min_age
+
+#     _, ord_to_cat, _  = age_mapping() 
+
+#     min_age = ord_to_cat[min_age  ]
+#     max_age = ord_to_cat[max_age]
+    
 
 
-    return  ignore_median, modal_median, modal_age , min_age, max_age, range_age,  distinct_ages,  ignore_iqr, modal_iqr
+#     return  ignore_median, modal_median, modal_age , min_age, max_age, range_age,  distinct_ages,  ignore_iqr, modal_iqr
 
 
 
-def generate_nulls(cols, pc):
-    dc = {'postcode': pc, 'count_unknown_age': np.nan  }
-    for col in cols:
-        dc[col] = np.nan 
-    return dc
 
-def process_postcode_age_residential(pc, data, INPUT_GPK):
+def process_postcode_age_residential(pc, data, INPUT_GPK, premise_dict):
     """Process one postcode, """
-    cols = [ 'ignorefill_median_age', 'modalfill_median_age', 'modal_age', 'min_age', 'max_age', 'range_age', 'distinct_ages', 'ignorefill_iqr_age', 'modalfill_iqr_age']
+    
+    def all_unknowns(df):   
+        if len(df[df['premise_type']==None])==len(df):
+            print('All unknowns')
+            dc = generate_nulls(cols, pc, prefixes)
+            return dc
+        else:
+            dicc= {} 
+            print('All unknowns but some type')    
+            df['global_fill_age'] = np.where(df['premise_age_bucketed'] == 'Unknown date', df['premise_type'].map(premise_dict), df['premise_age_bucketed'])
+            df['global_ordinal'] = df['global_fill_age'].map(category_to_ordinal)   
+            dc = calc_age_attributes(df, 'globalfill')
+            for i, col in enumerate(cols ) :
+                dicc[f'globalfill_{col}'] = dc[i]
+            dc_local = generate_nulls(cols, pc, ['localfill_'])
+            for i , col in enumerate(cols):
+                dicc[f'localfill_{col}'] = dc_local[i]
+            return dicc
+    
+    def normal_df(df):
+        df['loc_fill_age'] = np.where(df['premise_age_bucketed'] == 'Unknown date', df['premise_age_bucketed'].mode()[0], df['premise_age_bucketed'])
+        df['loc_ordinal'] = df['loc_fill_age'].map(category_to_ordinal)
+        df['global_fill_age'] = np.where(df['premise_age_bucketed'] == 'Unknown date', df['premise_type'].map(premise_dict), df['premise_age_bucketed'])
+        df['global_ordinal'] = df['global_fill_age'].map(category_to_ordinal)   
+        
+        dicc= {} 
+        for prefix in prefixes:
+            dc = calc_age_attributes(df, prefix)
+            for i, col in enumerate(cols ) :
+                dicc[f'{prefix}_{col}'] = dc[i]
+    
+    cols = [ 'median_age', 'modal_age', 'min_age', 'max_age', 'range_age', 'distinct_ages', 'iqr_age' ]
+    prefixes = ['localfill', 'globalfill']
+    
+
+    category_to_ordinal, _, _  = age_mapping()
     
     uprn_match = find_data_pc(pc, data, input_gpk=INPUT_GPK)
     
     if uprn_match is None or len(uprn_match) == 0:
-        dc= generate_nulls(cols, pc)
+        dc= generate_nulls(cols, pc, prefixes)
         return dc 
        
     # Generate building metrics, clean and test
@@ -146,34 +187,125 @@ def process_postcode_age_residential(pc, data, INPUT_GPK):
     # only calc for wholly residential 
     if len(df[df['premise_use']=='Residential']) != len(df):
         print('Not wholly residential')
-        dc= generate_nulls(cols, pc)
+        dc= generate_nulls(cols, pc, prefixes)
         return dc
 
-    
     if check_duplicate_primary_key(df, 'upn'):
         raise Exception('Duplicate primary key found for upn')
-
+    
+    if len(df)==1:
+        dc= generate_nulls(cols, pc, prefixes)
+        return dc
+    
     df = bucket_age(df)
     count_unknown = df[df['premise_age_bucketed']=='Unknown date'].shape[0]
-
     dc_full = {'postcode': pc, 'count_unknown_age': count_unknown  }     
-    
-    dicc= {} 
-    dc = calc_age_attributes(df)
-    for i, col in enumerate(cols ) :
-        dicc[col] = dc[i]
+
+    if len(df[df['premise_age']=='Unknown date']) ==len(df):
+        print('Starting all unknown postcode')
+        dicc = all_unknowns(df)
+    else:
+        dicc= normal_df(df)
 
     dc_full.update(dicc )
-            
+   
     return dc_full 
 
 
+def generate_nulls(cols, pc, prefixes):
+    dc = {'postcode': pc, 'count_unknown_age': np.nan  }
+    for prefix in prefixes:
+        for col in cols:
+            dc[f'{prefix}_{col}'] = np.nan 
+    return dc
+
  
-def load_global_average_mode():
-    """ Load global average data """
-    global_av = pd.read_csv('/Users/gracecolverd/New_dataset/global_averages/global_Average_age_df_mode.csv')
-    return  bucket_age(global_av)    
+
+
+        
+
+
+
+
+
+# def process_postcode_age_residential(pc, data, INPUT_GPK):
+#     """Process one postcode, """
+#     cols = [ 'ignorefill_median_age', 'modalfill_median_age', 'modal_age', 'min_age', 'max_age', 'range_age', 'distinct_ages', 'ignorefill_iqr_age', 'modalfill_iqr_age']
     
+#     uprn_match = find_data_pc(pc, data, input_gpk=INPUT_GPK)
+    
+#     if uprn_match is None or len(uprn_match) == 0:
+#         dc= generate_nulls(cols, pc)
+#         return dc 
+       
+#     # Generate building metrics, clean and test
+#     df  = pre_process_buildings(uprn_match)    
+#     # only calc for wholly residential 
+#     if len(df[df['premise_use']=='Residential']) != len(df):
+#         print('Not wholly residential')
+#         dc= generate_nulls(cols, pc)
+#         return dc
+
+    
+#     if check_duplicate_primary_key(df, 'upn'):
+#         raise Exception('Duplicate primary key found for upn')
+
+#     df = bucket_age(df)
+#     count_unknown = df[df['premise_age_bucketed']=='Unknown date'].shape[0]
+
+#     dc_full = {'postcode': pc, 'count_unknown_age': count_unknown  }     
+    
+#     dicc= {} 
+#     dc = calc_age_attributes(df)
+#     for i, col in enumerate(cols ) :
+#         dicc[col] = dc[i]
+
+#     dc_full.update(dicc )
+            
+#     return dc_full 
 
 
+# def calculate_median_age_band(df):
+#     """
+#     Calculate the median of a categorical column based on a specified ordering.
 
+#     Parameters:
+#     - df: pandas DataFrame containing the data.
+#     - modal_ignore: str, whether to use the modal value for unknown dates ('modal') or ignore unknown dates ('ignore').
+
+#     Returns:
+#     - The category that represents the median.
+#     """
+#     def calc_med(df):
+#         ordinal_values = df['premise_age_bucketed'].map(category_to_ordinal)
+#         median_ordinal = ordinal_values.median()
+#         median_category = category_order[int(np.round(median_ordinal))]
+#         return median_category
+
+#     def calc_iqr(df):
+#         q1 =  df['ordinal_age'].quantile(0.25)
+#         q3 = df['ordinal_age'].quantile(0.75)
+#         iqr = q3 - q1
+#         return iqr 
+
+#     category_to_ordinal, _ , category_order = age_mapping() 
+#     df_ignore  = df[df['premise_age_bucketed']!='Unknown date'].copy() 
+
+#     df['premise_age_bucketed'] = df['premise_age_bucketed'].replace('Unknown date', df['premise_age_bucketed'].mode()[0])    
+
+#     if len(df_ignore) == 0:
+#         print('No data for ignore')
+#         ignore_median = np.nan 
+#     else:
+#         ignore_median = calc_med(df_ignore)
+
+#     if len(df) == 0:
+#         modal_median = np.nan
+#     else:
+#         modal_median = calc_med(df)
+
+#     ignore_iqr = calc_iqr(df_ignore)
+#     modal_iqr = calc_iqr(df)
+
+#     return ignore_median, modal_median, ignore_iqr, modal_iqr
+        
--- src/age_proc.py
+++ src/age_proc.py
@@ -1,16 +1,37 @@
 import pandas as pd 
 import os 
-from src.age_calc import process_postcode_age_residential
+from src.age_calc import process_postcode_age_residential, bucket_age
+
+def load_global_average_mode():
+    """ Load global average data """
+    current_dir = os.path.dirname(__file__)
+    csv_path = os.path.join(current_dir, 'global_avs', 'global_Average_age_df_mode.csv')
+    global_av = pd.read_csv(csv_path)
+    global_av =  bucket_age(global_av)    
+    premise_dict = {}
+
+    for i, row in global_av.iterrows():
+        premise_type = row['premise_type']
+        premise_age_bucketed = row['premise_age_bucketed']
+
+        if premise_type not in premise_dict:
+            premise_dict[premise_type] = {}
+        
+        premise_dict[premise_type] = premise_age_bucketed
+    premise_dict['Unknown date'] = 'Post 1999'   
+    return premise_dict 
+
 
 def process_age_batch(pc_batch, data, INPUT_GPK, temp_dir, process_batch_name):
     print('Starting batch processing...')
     log_file = os.path.join(temp_dir, f'{process_batch_name}_log_file.csv')
-    
+    print('Loading global average data...')
+    premise_dict = load_global_average_mode()
     # Initialize an empty list to collect results
     results = []
     for pc in pc_batch:
         print('Processing postcode:', pc)
-        pc_result = process_postcode_age_residential(pc, data, INPUT_GPK)  # Assuming this function is defined elsewhere
+        pc_result = process_postcode_age_residential(pc, data, INPUT_GPK, premise_dict)  # Assuming this function is defined elsewhere
         if pc_result is not None:
             results.append(pc_result)
     
