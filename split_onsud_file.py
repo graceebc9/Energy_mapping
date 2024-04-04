8dfbe4a2eec121d8fff69a857ed413693fd4cdf5
Grace Colverd <gb669@cam.ac.uk>
2024-04-04 11:22:14 +0100

    add onsud split

--- split_onsud_file.py
+++ split_onsud_file.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import os 
-from src.fuel_proc import  load_onsud_data
+from src.postcode_utils import  load_onsud_data
 
 
 def split_onsud_file(path_to_onsud_file, path_to_pcshp, output_directory, logfile, label  ):
@@ -25,13 +25,17 @@ def split_onsud_file(path_to_onsud_file, path_to_pcshp, output_directory, logfil
         os.makedirs(batch_dir, exist_ok=True)   
 
         batch_filename = os.path.join(batch_dir, f"batch_{i//batch_size}.txt" ) 
-
+        
         with open('batch_paths.txt', 'w') as f:
                 f.write(f"{batch_filename}\n")
 
-        with open(batch_filename, 'w') as f:
+        with open(batch_filename, 'a') as f:
             for pc in batchs:
                 f.write(f"{pc}\n")
+
+        # export subst of onsud file 
+        subsetdata = onsud_data[onsud_data['PCDS'].isin(batchs)].copy()
+        subsetdata.to_csv(f'{batch_dir}/onsud_{i//batch_size}.csv', index=False)
 
     print('Batches saved')
    
@@ -45,15 +49,15 @@ if __name__ == "__main__":
     label = path_to_onsud_file.split('/')[-1].split('.')[0].split('_')[-1]
     log = os.path.join(output_directory, label,  'log_file.csv')
     
-    batch_list = split_onsud_file(path_to_onsud_file, path_to_pcshp, output_directory,logfile =  log , label =  label)
+    split_onsud_file(path_to_onsud_file, path_to_pcshp, output_directory,logfile =  log , label =  label)
     
-    print('Batches saved to ', batch_list)
+    print('Batches saved to ')
 
 
 
 
 
-#     export DATA_DIR='/Users/gracecolverd/New_dataset/old/testing'
-# export ONSUD_PATH='/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022/Data/ONSUD_DEC_2022_EE.csv'
+# export OUTPUT_DIR='/Users/gracecolverd/New_dataset/test'
+# export ONSUD_PATH='/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022/Data/ONSUD_DEC_2022_LN.csv'
 # export PC_SHP_PATH='/Volumes/T9/Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998/codepoint-poly_5267291'
 
