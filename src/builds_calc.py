a112f7cf9ff43760e6bdfb5824d66398bcb8a700
Grace Colverd <gb669@cam.ac.uk>
2024-04-30 16:04:57 +0100

    add type calc

--- src/builds_calc.py
+++ src/builds_calc.py
@@ -9,10 +9,33 @@ import sys
 import numpy as np  
 
 
-def calc_build_attr(df):
-    df[df['']]
 
-def process_postcode(pc, onsud_data, INPUT_GPK, overlap = False, batch_dir=None, path_to_pcshp=None  ):
+
+def calc_filtered_percentage_of_premise_type(df, prem_types):
+    """
+    Function to create percentage of different building types, filtered by specified types,
+    and return it in a dictionary.
+    
+    Parameters:
+    df : DataFrame containing one postcode's worth of building information.
+    prem_types : List of building types to include in the output.
+    """
+    # Calculate the count of each type and normalize to get percentages
+    all_premise_types = df['premise_type'].value_counts()
+
+    # Filter to keep only the specified premise types
+    filtered_premise_types = all_premise_types[all_premise_types.index.isin(prem_types)]
+
+    return filtered_premise_types.to_dict()
+
+def calc_res_clean_counts(df):
+    res = df[df['premise_use']=='Residential']
+    return len(res)
+
+
+
+    
+def process_postcode(pc, onsud_data,  INPUT_GPK, overlap = False, batch_dir=None, path_to_pcshp=None  ):
     """Process one postcode, deriving building attributes and electricity and fuel info.
     
     Inputs: 
@@ -26,6 +49,26 @@ def process_postcode(pc, onsud_data, INPUT_GPK, overlap = False, batch_dir=None,
     batch_dir = needed for overlap - where are the batche stored?
     path_to_schp: path to postcode shapefiles location , needed for overlap 
     """
+    prem_types = ['Medium height flats 5-6 storeys',
+    'Small low terraces',
+    '3-4 storey and smaller flats',
+    'Tall terraces 3-4 storeys',
+    'Large semi detached',
+    'Standard size detached',
+    'Standard size semi detached',
+    '2 storeys terraces with t rear extension',
+    'Semi type house in multiples',
+    'Tall flats 6-15 storeys',
+    'Large detached',
+    'Very tall point block flats',
+    'Very large detached',
+    'Planned balanced mixed estates',
+    'Linked and step linked premises',
+    'Domestic outbuilding',
+    'Unknown'
+    ]
+
+
     pc = pc.strip() 
 
     if overlap ==True: 
@@ -37,6 +80,10 @@ def process_postcode(pc, onsud_data, INPUT_GPK, overlap = False, batch_dir=None,
     uprn_match= find_data_pc_joint(pc, onsud_data, input_gpk=INPUT_GPK)
     dc_full = {'postcode': pc  }
 
+    for val in prem_types:
+        dc_full[val] = np.nan
+    dc_full['len_res'] = np.nan 
+
     if uprn_match.empty:
         print('Empty uprn match')
         dc =  gen_nulls()
@@ -45,11 +92,15 @@ def process_postcode(pc, onsud_data, INPUT_GPK, overlap = False, batch_dir=None,
         df  = pre_process_building_data(uprn_match)    
         if len(df)!=len(uprn_match):
             raise Exception('Error in pre process - some cols dropped? ')
-        dc = dummm(df)
+        dc = calc_filtered_percentage_of_premise_type(df, prem_types)
         if df is not None:
             if check_duplicate_primary_key(df, 'upn'):
                 print('Duplicate primary key found for upn')
                 sys.exit()
-    dc_full.update(dc)
+    
 
+    dc_res = {'len_res' : calc_res_clean_counts(uprn_match) }
+    dc_full.update(dc_res)
+    dc_full.update(dc)
+    
     return dc_full
