889a0d8d6aa719ed5f2e1638bd75b2913bb9b595
“grace <gb669@cam.ac.uk>
2024-05-08 22:32:00 +0100

    chang target var

--- run_automl.py
+++ run_automl.py
@@ -9,6 +9,10 @@ from autogluon.tabular import TabularDataset, TabularPredictor
 import os
 import sys
 
+# # set random seed 
+# import random
+# random.seed(42) 
+
 def check_directory_and_files(output_directory, required_files):
     """
     Check if the specified directory exists and contains the required files.
@@ -37,10 +41,12 @@ def check_directory_and_files(output_directory, required_files):
 
 def transform(df, label):
     cols_remove = [
+        'av_gas_per_vol',
+        # 'total_gas',
         'ï»¿pcd7', 'pcd8', 'pcds', 'dointr', 'doterm', 'usertype', 'oa21cd',
         'lsoa21cd', 'msoa21cd', 'ladcd', 'lsoa21nm', 'msoa21nm', 'ladnm',
         'ladnmw', 'pcd7', 'diff_min_max_gas_per_vol', 'diff_gas_meters_uprns_res',
-        'min_gas_per_vol', 'max_gas_per_vol', 'total_gas', 'avg_gas', 'median_gas',
+        'min_gas_per_vol', 'max_gas_per_vol',  'avg_gas', 'median_gas',
         'num_meters_gas', 'total_elec', 'avg_elec', 'median_elec', 'num_meters_elec',
         'comm_alltypes_count', 'unknown_alltypes', 'outb_res_uprn_count_total', 'mixed_total_buildings', 'mixed_premise_area_total', 'mixed_gross_area_total', 'mixed_heated_vol_fc_total', 'mixed_heated_vol_h_total', 'mixed_base_floor_total', 'mixed_basement_heated_vol_max_total', 'mixed_listed_bool_total', 'mixed_uprn_count_total', 'percent_residential', 'perc_all_res', 'Unknown_pct', 'ethnic_group_perc_Does not apply', 'household_siz_perc_perc_0 people in household', 'occupancy_rating_perc_Does not apply', 'household_comp_by_bedroom_perc_Does not apply_Does not apply', 'household_comp_by_bedroom_perc_Does not apply_1 bedroom', 'household_comp_by_bedroom_perc_Does not apply_2 bedrooms', 'household_comp_by_bedroom_perc_Does not apply_3 bedrooms', 'household_comp_by_bedroom_perc_Does not apply_4 or more bedrooms', 'household_comp_by_bedroom_perc_One-person household_Does not apply', 'household_comp_by_bedroom_perc_Single family household: All aged 66 years and over_Does not apply', 'household_comp_by_bedroom_perc_Single family household: Couple family household_Does not apply', 'household_comp_by_bedroom_perc_Single family household: Lone parent household_Does not apply', 'household_comp_by_bedroom_perc_Other household types_Does not apply', 'central_heating_perc_Does not apply'
     ]
@@ -57,15 +63,24 @@ def save_results(results, output_path):
 
 
 def main():
+    # train_path = os.environ.get('TRAIN_PATH')
+    # test_path = os.environ.get('TEST_PATH')
     data_path = os.environ.get('DATA_PATH')
     output_path = os.environ.get('OUTPUT_PATH')
     model_preset= os.environ.get('MODEL_PRESET')
     time_limit = int(os.environ.get('TIME_LIM'))
     train_subset_prop = float(os.environ.get('TRAIN_SUBSET_PROP') )
-    
-    label = 'av_gas_per_vol'
-    target_var='avgas'    
+    model_types = os.environ.get('MODEL_TYPES')
+
+    label = 'total_gas'
+    # label = 'av_gas_per_vol'
+    target_var=label    
     model_names ='all'
+
+    if model_types == 'all':
+        excl_models = []
+    elif model_types=='set1':
+        excl_models = ['KNN', 'GBM', 'CAT', 'RF', 'XT']
 
     print(f'starting model run for {target_var}, time lim {time_limit}, model preset {model_preset} adn train subset {train_subset_prop}' )
 
@@ -74,19 +89,17 @@ def main():
 
 
 
-    if not data_path or not output_path:
+    if not data_path or not output_path or not data_path:
         print("Please set DATA_PATH and OUTPUT_PATH environment variables.")
         sys.exit(1)
 
-    dataset_name = os.path.basename(data_path).split('.')[0]
-    output_directory = f"{output_path}/{dataset_name}_{target_var}_{time_limit}_{model_preset}_{col_type}_tsp_{train_subset_prop}_{model_names}"
+    dataset_name = os.path.basename(data_path).split('.')[0].split('_tr')[0]
+    output_directory = f"{output_path}/{dataset_name}_{target_var}_{time_limit}_{model_preset}_{col_type}_tsp_{train_subset_prop}_{model_types}"
     
     # Example usage:
     
     required_files = ['model_summary.txt', 'feature_importance.csv', 'leaderboard_results.csv']  # List of files you expect to exist
-    excluded_model_types = ['KNN', 
-                            
-                            , 'CatBoost', 'ExtraTreesMSE', 'LightGBM', 'LightGBMXT']
+    
     
     # Check if output directory exists and has all required files
     if check_directory_and_files(output_directory, required_files):
@@ -116,7 +129,7 @@ def main():
                                                                 #    num_gpus=1,
                                                                 time_limit=time_limit,
                                                                 presets=model_preset,
-                                                                excluded_model_types=excluded_model_types)
+                                                                excluded_model_types=excl_models)
     
     test_data = transform(TabularDataset(test_data), label)
     y_pred = predictor.predict(test_data.drop(columns=[label]))
@@ -139,7 +152,8 @@ if __name__ == '__main__':
 
 
 
-# export DATA_PATH='/Volumes/T9/Data_downloads/new-data-outputs/ml_data_engwales_census_v2.csv'
+# export TRAIN_PATH='
+# export TEST_PATH='
 # export OUTPUT_PATH='/Volumes/T9/Data_downloads/new-data-outputs/ml/results'
 # export MODEL_PRESET='medium_quality'
 # export TIME_LIM=500
