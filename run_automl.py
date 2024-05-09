b34bc6829aa4c6ad7ab55a55362461ed5b8446dc
“grace <gb669@cam.ac.uk>
2024-05-09 09:56:52 +0100

    add run_automl

--- run_automl.py
+++ run_automl.py
@@ -41,15 +41,19 @@ def check_directory_and_files(output_directory, required_files):
 
 def transform(df, label):
     cols_remove = [
-        'av_gas_per_vol',
-        # 'total_gas',
-        'ï»¿pcd7', 'pcd8', 'pcds', 'dointr', 'doterm', 'usertype', 'oa21cd',
-        'lsoa21cd', 'msoa21cd', 'ladcd', 'lsoa21nm', 'msoa21nm', 'ladnm',
-        'ladnmw', 'pcd7', 'diff_min_max_gas_per_vol', 'diff_gas_meters_uprns_res',
-        'min_gas_per_vol', 'max_gas_per_vol',  'avg_gas', 'median_gas',
-        'num_meters_gas', 'total_elec', 'avg_elec', 'median_elec', 'num_meters_elec',
-        'comm_alltypes_count', 'unknown_alltypes', 'outb_res_uprn_count_total', 'mixed_total_buildings', 'mixed_premise_area_total', 'mixed_gross_area_total', 'mixed_heated_vol_fc_total', 'mixed_heated_vol_h_total', 'mixed_base_floor_total', 'mixed_basement_heated_vol_max_total', 'mixed_listed_bool_total', 'mixed_uprn_count_total', 'percent_residential', 'perc_all_res', 'Unknown_pct', 'ethnic_group_perc_Does not apply', 'household_siz_perc_perc_0 people in household', 'occupancy_rating_perc_Does not apply', 'household_comp_by_bedroom_perc_Does not apply_Does not apply', 'household_comp_by_bedroom_perc_Does not apply_1 bedroom', 'household_comp_by_bedroom_perc_Does not apply_2 bedrooms', 'household_comp_by_bedroom_perc_Does not apply_3 bedrooms', 'household_comp_by_bedroom_perc_Does not apply_4 or more bedrooms', 'household_comp_by_bedroom_perc_One-person household_Does not apply', 'household_comp_by_bedroom_perc_Single family household: All aged 66 years and over_Does not apply', 'household_comp_by_bedroom_perc_Single family household: Couple family household_Does not apply', 'household_comp_by_bedroom_perc_Single family household: Lone parent household_Does not apply', 'household_comp_by_bedroom_perc_Other household types_Does not apply', 'central_heating_perc_Does not apply'
-    ]
+            'Unnamed: 0', 'index', 'POSTCODE', 'postcode',
+            'ï»¿pcd7', 'pcd8', 'pcds', 'dointr', 'doterm', 'usertype', 'oa21cd',
+            'lsoa21cd', 'msoa21cd', 'ladcd', 'lsoa21nm', 'msoa21nm', 'ladnm',
+            'ladnmw', 'pcd7', 'diff_min_max_gas_per_vol', 'diff_gas_meters_uprns_res',
+            'min_gas_per_vol', 'max_gas_per_vol',  'avg_gas', 'median_gas',
+            'num_meters_gas', 'total_elec', 'avg_elec', 'median_elec', 'num_meters_elec',
+            'comm_alltypes_count', 'unknown_alltypes', 'outb_res_uprn_count_total', 'mixed_total_buildings', 'mixed_premise_area_total', 'mixed_gross_area_total', 'mixed_heated_vol_fc_total', 'mixed_heated_vol_h_total', 'mixed_base_floor_total', 'mixed_basement_heated_vol_max_total', 'mixed_listed_bool_total', 'mixed_uprn_count_total', 'percent_residential', 'perc_all_res', 'Unknown_pct', 'ethnic_group_perc_Does not apply', 'household_siz_perc_perc_0 people in household', 'occupancy_rating_perc_Does not apply', 'household_comp_by_bedroom_perc_Does not apply_Does not apply', 'household_comp_by_bedroom_perc_Does not apply_1 bedroom', 'household_comp_by_bedroom_perc_Does not apply_2 bedrooms', 'household_comp_by_bedroom_perc_Does not apply_3 bedrooms', 'household_comp_by_bedroom_perc_Does not apply_4 or more bedrooms', 'household_comp_by_bedroom_perc_One-person household_Does not apply', 'household_comp_by_bedroom_perc_Single family household: All aged 66 years and over_Does not apply', 'household_comp_by_bedroom_perc_Single family household: Couple family household_Does not apply', 'household_comp_by_bedroom_perc_Single family household: Lone parent household_Does not apply', 'household_comp_by_bedroom_perc_Other household types_Does not apply', 'central_heating_perc_Does not apply'
+        ]
+    if label == 'av_gas_per_vol':
+        cols_remove+=['total_gas']
+    elif label == 'total_gas':
+        cols_remove += ['av_gas_per_vol'] 
+            
     working_cols = [col for col in df.columns if col not in cols_remove]
     df = df[working_cols]
     df = df[~df[label].isna()]
@@ -71,19 +75,24 @@ def main():
     time_limit = int(os.environ.get('TIME_LIM'))
     train_subset_prop = float(os.environ.get('TRAIN_SUBSET_PROP') )
     model_types = os.environ.get('MODEL_TYPES')
+    target = os.environ.get('TARGET')
 
-    label = 'total_gas'
-    # label = 'av_gas_per_vol'
+    if target == 'avgas':   
+        label = 'av_gas_per_vol'
+    elif target == 'totalgas':
+        label = 'total_gas'
+
+    
     target_var=label    
-    model_names ='all'
+    # model_names ='all'
     excl_models = [] 
 
     if model_types == 'all':
         excl_models = []
     elif model_types=='set1':
-        excl_models = ['KNN', 'GBM', 'CAT', 'RF', 'XT']
+        excl_models = ['KNN', 'GBM', 'CAT', 'RF', 'XT', 'NN_TORCH']
 
-    print(f'starting model run for {target_var}, time lim {time_limit}, model preset {model_preset} adn train subset {train_subset_prop}' )
+    print(f'starting model run for {target_var}, time lim {time_limit}, model preset {model_preset} and train subset {train_subset_prop}' )
 
       # Proportion of data to use for training
     col_type ='allcols'
@@ -117,7 +126,7 @@ def main():
     # train_data.to_csv(os.path.join(output_directory, 'train_data.csv'), index=False)
     test_data.to_csv(os.path.join(output_directory, 'test_data.csv'), index=False)
 
-    train_data = transform(TabularDataset(train_data), label)
+    train_data = transform(TabularDataset(train_data), label, )
     
     # Reduce the training dataset if needed
     if train_subset_prop != 1:
