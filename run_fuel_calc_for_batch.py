238370e61053529461a860e34083a3e2b99a0038
“grace <gb669@cam.ac.uk>
2024-05-31 21:59:31 +0100

    re tool main fuel calc to do overlap w onsud file for overlaps (largemem)

--- run_fuel_calc_for_batch.py
+++ run_fuel_calc_for_batch.py
@@ -17,23 +17,33 @@ if __name__ == "__main__":
     path_to_pcshp= os.environ.get('PC_SHP_PATH')
     input_gpk_building = os.environ.get('BUILDING_PATH')
     batch_path = os.environ.get('BATCH_PATH') 
-    
-    label = batch_path.split('/')[-2]
-    batch_id = batch_path.split('/')[-1].split('.')[0].split('_')[-1]
+    overlap = os.environ.get('OVERLAP_BL')
+ 
     # path_to_onsud_file = get_onsud_path( onsud_dir, onsud_data, label )
-    onsud_path = os.path.join(os.path.dirname(batch_path), f'onsud_{batch_id}.csv') 
+    print(overlap)
+    if overlap=='Yes':
+        print('overlap starting')
+        onsud_path = os.environ.get('OVERLAP_ONSUD_PATH')
+        label='overlap'
+        batch_id = 'overlap'
+    else:
+        print('Non overlap starting')
+        onsud_path = os.path.join(os.path.dirname(batch_path), f'onsud_{batch_id}.csv') 
+        label = batch_path.split('/')[-2]
+        batch_id = batch_path.split('/')[-1].split('.')[0].split('_')[-1]
     print(onsud_path)
     main(batch_path, data_dir, onsud_path, path_to_pcshp, INPUT_GPK=input_gpk_building, region_label=label,  batch_label=batch_id, attr_lab='fuel', process_function=run_fuel_process, gas_path=gas_path, elec_path=elec_path)
 
 
 
 
-# export  BATCH_PATH='/Users/gracecolverd/New_dataset/test/batches/WM/batch_0.txt'
+# # export  BATCH_PATH='/Users/gracecolverd/New_dataset/test/batches/WM/batch_0.txt'
+# BATCH_PATH='/Users/gracecolverd/New_dataset/overlapping_pcs.txt'
 # export DATA_DIR='/Users/gracecolverd/New_dataset/test'
-# export ONSUD_DIR='/Volumes/T9/Data_downloads/ONS_UPRN_database/ONSUD_DEC_2022'
 # export PC_SHP_PATH='/Volumes/T9/Data_downloads/codepoint_polygons_edina/Download_all_postcodes_2378998/codepoint-poly_5267291'
 # export GAS_PATH='/Volumes/T9/Data_downloads/UKGOV_Gas_elec/Postcode_level_gas_2022.csv'
 # export ELEC_PATH='/Volumes/T9/Data_downloads/UKGOV_Gas_elec/Postcode_level_all_meters_electricity_2022.csv'
 # export BUILDING_PATH='/Volumes/T9/Data_downloads/Versik_building_data/2024_03_22_updated_data/UKBuildings_Edition_15_new_format_upn.gpkg'
-    
+# export OVERLAP_ONSUD_PATH='/Users/gracecolverd/New_dataset/postcode_attrs/ONSUD_Data_OVERLAP.csv'
+# export OVERLAP_BL='Yes'
     
