178e89d6578e3c1416647e32270b7743912c782e
“grace <gb669@cam.ac.uk>
2024-06-05 13:37:13 +0100

    fix bug

--- ml_utils/src/model_col_final.py
+++ ml_utils/src/model_col_final.py
@@ -307,7 +307,7 @@ settings_dict = {
 20: ['NDVI', ndvi_cols], 
 21: ['COB + NDVI' , total_builds_new + ndvi_cols],
 22: ['COB, NDVI, Temp, Urban Rural' , total_builds_new + ndvi_cols + temp_cols + rural_urban], 
-23: ['COB, NDVI, Temp, Urban Rural, Postcode Geoms' : total_builds_new + ndvi_cols + temp_cols + rural_urban + postcode_geoms ], 
-24: ['COB, NDVI, Temp, Urban Rural, Postcode Geoms, Type' : total_builds_new + ndvi_cols + temp_cols + rural_urban + postcode_geoms + type_cols ], 
-25: ['COB, NDVI, Temp, Urban Rural, Postcode Geoms, Census' : total_builds_new + ndvi_cols + temp_cols + rural_urban + postcode_geoms + all_census ], 
+23: ['COB, NDVI, Temp, Urban Rural, Postcode Geoms' , total_builds_new + ndvi_cols + temp_cols + rural_urban + postcode_geoms ], 
+24: ['COB, NDVI, Temp, Urban Rural, Postcode Geoms, Type' ,  total_builds_new + ndvi_cols + temp_cols + rural_urban + postcode_geoms + type_cols ], 
+25: ['COB, NDVI, Temp, Urban Rural, Postcode Geoms, Census' , total_builds_new + ndvi_cols + temp_cols + rural_urban + postcode_geoms + all_census ], 
 }
