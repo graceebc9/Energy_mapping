e574900904f141edc7db88e07189a7d7d8a0deb1
“grace <gb669@cam.ac.uk>
2024-10-18 12:10:45 +0100

    retool for dynamic

--- ml_utils/src/problem_definitions.py
+++ ml_utils/src/problem_definitions.py
@@ -1,212 +1,94 @@
 # problem_definitions.py
+from .model_col_final import settings_dict
 
-problems = {
-    47: {
-        
-        'num_vars': 18,
-         'names': [
-                'all_res_heated_vol_h_total', 
-                'clean_res_total_buildings', 
-                'clean_res_heated_vol_h_total',
-                'Domestic outbuilding_pct', 
-                'Standard size detached_pct', 
-                'Standard size semi detached_pct',
-                'Pre 1919_pct', 
-                'Unknown_age_pct', 
-                '1960-1979_pct', 
-                'HDD', 
-                'CDD', 
-                'HDD_winter', 
-                'postcode_area', 
-                'postcode_density', 
-                'log_pc_area', 
-                'ethnic_group_perc_White: English, Welsh, Scottish, Northern Irish or British',
-                'central_heating_perc_Mains gas only',
-                'Average Household Size'
-             ], 
-        'bounds': [
-                    [0, 7600], 
-                    [0, 50], 
-                    [0, 7400],
-                    [0, 52],
-                    [0, 100], 
-                    [0, 100],
-                    [0, 100], 
-                    [0, 20],
-                    [0, 100], 
-                    [30, 80],
-                    [0, 8],
-                    [30, 60],
-                    [1, 26000], 
-                    [0, 0.5], 
-                    [5, 12], 
-                    [0, 1], 
-                    [0, 1], 
-                    [1, 5]]
-                   }
+# Base dictionary of all possible parameters and their bounds
+base_params = {
+    'all_res_heated_vol_h_total': [0, 7600],
+    'all_types_total_buildings': [0, 150],
+    'clean_res_total_buildings': [0, 150],
+    'clean_res_heated_vol_h_total': [0, 7400],
+    'Domestic outbuilding_pct': [0, 52],
+    'Standard size detached_pct': [0, 100],
+    'Standard size semi detached_pct': [0, 100],
+    'Small low terraces_pct': [0, 100],
+    '2 storeys terraces with t rear extension_pct': [0, 100],
+    'Pre 1919_pct': [0, 100],
+    'Unknown_age_pct': [0, 20],
+    '1960-1979_pct': [0, 100],
+    '1919-1944_pct': [0, 100],
+    'Post 1999_pct': [0, 100],
+    'HDD': [30, 80],
+    'CDD': [0, 8],
+    'HDD_summer': [3, 15],
+    'HDD_winter': [30, 60],
+    'postcode_area': [1, 26000],
+    'postcode_density': [0, 0.5],
+    'log_pc_area': [5, 12],
+    'ethnic_group_perc_White: English, Welsh, Scottish, Northern Irish or British': [0, 1],
+    'central_heating_perc_Mains gas only': [0, 1],
+    'household_siz_perc_perc_1 person in household': [0, 1],
+    'Average Household Size': [1, 5],
+    'clean_res_premise_area_total': [0, 2000],
+    'all_res_premise_area_total': [0, 2000],
+    'all_res_base_floor_total': [0, 1000]
+}
+
+def generate_problem(col_setting):
+    name, cols = settings_dict[col_setting]
+    
+    new_problem = {
+        'num_vars': len(cols),
+        'names': cols,
+        'bounds': [base_params[col] for col in cols]
     }
-    45: {
-        'num_vars': 13,
-        'names': [
-            'all_res_heated_vol_h_total',
-            'clean_res_total_buildings',
-            'Domestic outbuilding_pct',
-            'Standard size detached_pct',
-            'Standard size semi detached_pct',
-            'Pre 1919_pct',
-            'Unknown_age_pct',
-            'HDD',
-            'CDD',
-            'postcode_density',
-            'log_pc_area',
-            'ethnic_group_perc_White: English, Welsh, Scottish, Northern Irish or British',
-            'central_heating_perc_Mains gas only'
-        ],
-        'bounds': [
-            [0, 7600],  # all_res_heated_vol_h_total
-            [0, 50],    # clean_res_total_buildings
-            [0, 52],    # Domestic outbuilding_pct
-            [0, 100],   # Standard size detached_pct
-            [0, 100],   # Standard size semi detached_pct
-            [0, 100],   # Pre 1919_pct
-            [0, 20],    # Unknown_age_pct
-            [30, 80],   # HDD
-            [0, 8],     # CDD
-            [0, 0.5],   # postcode_density
-            [5, 12],    # log_pc_area
-            [0, 1],     # ethnic_group_perc_White: English, Welsh, Scottish, Northern Irish or British
-            [0, 1]      # central_heating_perc_Mains gas only
-        ]
-    },
-    44: {
-        'ungrouped': {
-            'num_vars': 26,
-            'names': [
-                'all_res_heated_vol_h_total',
-                'clean_res_total_buildings',
-                'clean_res_heated_vol_h_total',
-                'Domestic outbuilding_pct',
-                'Standard size detached_pct',
-                'Standard size semi detached_pct',
-                'Small low terraces_pct',
-                '2 storeys terraces with t rear extension_pct',
-                'Pre 1919_pct',
-                'Unknown_age_pct',
-                '1960-1979_pct',
-                '1919-1944_pct',
-                'Post 1999_pct',
-                'HDD',
-                'CDD',
-                'HDD_summer',
-                'HDD_winter',
-                'postcode_area',
-                'postcode_density',
-                'log_pc_area',
-                'ethnic_group_perc_White: English, Welsh, Scottish, Northern Irish or British',
-                'central_heating_perc_Mains gas only',
-                'household_siz_perc_perc_1 person in household',
-                'Average Household Size',
-                'clean_res_premise_area_total',
-                'all_res_base_floor_total'
-            ],
-            'bounds': [
-                [0, 7600],  # all_res_heated_vol_h_total
-                [0, 50],    # clean_res_total_buildings
-                [0, 7400],  # clean_res_heated_vol_h_total
-                [0, 52],    # Domestic outbuilding_pct
-                [0, 100],   # Standard size detached_pct
-                [0, 100],   # Standard size semi detached_pct
-                [0, 100],   # Small low terraces_pct
-                [0, 100],   # 2 storeys terraces with t rear extension_pct
-                [0, 100],   # Pre 1919_pct
-                [0, 20],    # Unknown_age_pct
-                [0, 100],   # 1960-1979_pct
-                [0, 100],   # 1919-1944_pct
-                [0, 100],   # Post 1999_pct
-                [30, 80],   # HDD
-                [0, 8],     # CDD
-                [3, 15],    # HDD_summer
-                [30, 60],   # HDD_winter
-                [1, 26000], # postcode_area
-                [0, 0.5],   # postcode_density
-                [5, 12],    # log_pc_area
-                [0, 1],     # ethnic_group_perc_White: English, Welsh, Scottish, Northern Irish or British
-                [0, 1],     # central_heating_perc_Mains gas only
-                [0, 1],     # household_siz_perc_perc_1 person in household
-                [1, 5],     # Average Household Size
-                [0, 2000],  # clean_res_premise_area_total
-                [0, 1000]   # all_res_base_floor_total
-            ]
-        },
-        'grouped': {
-            'num_vars': 26,
-            'groups': ['group_1', 'group_1','group_1', 'group_2', 'group_3', 'group_4', 'group_5',  'group_6', 'group_7', 'group_8', 'group_9', 
-            'group_10', 'group_11', 'group_12', 'group_13',  'group_12', 'group_12' , 'group_14', 'group_15', 'group_14', 'group_15' , 
-            'group_16', 'group_17', 'group_18', 'group_1', 'group_1'],
-            'names': [
-                'all_res_heated_vol_h_total',
-                'clean_res_total_buildings',
-                'clean_res_heated_vol_h_total',
-                'Domestic outbuilding_pct',
-                'Standard size detached_pct',
-                'Standard size semi detached_pct',
-                'Small low terraces_pct',
-                '2 storeys terraces with t rear extension_pct',
-                'Pre 1919_pct',
-                'Unknown_age_pct',
-                '1960-1979_pct',
-                '1919-1944_pct',
-                'Post 1999_pct',
-                'HDD',
-                'CDD',
-                'HDD_summer',
-                'HDD_winter',
-                'postcode_area',
-                'postcode_density',
-                'log_pc_area',
-                'ethnic_group_perc_White: English, Welsh, Scottish, Northern Irish or British',
-                'central_heating_perc_Mains gas only',
-                'household_siz_perc_perc_1 person in household',
-                'Average Household Size',
-                'clean_res_premise_area_total',
-                'all_res_base_floor_total'
-            ],
-            'bounds': [
-                [0, 7600],  # all_res_heated_vol_h_total
-                [0, 50],    # clean_res_total_buildings
-                [0, 7400],  # clean_res_heated_vol_h_total
-                [0, 52],    # Domestic outbuilding_pct
-                [0, 100],   # Standard size detached_pct
-                [0, 100],   # Standard size semi detached_pct
-                [0, 100],   # Small low terraces_pct
-                [0, 100],   # 2 storeys terraces with t rear extension_pct
-                [0, 100],   # Pre 1919_pct
-                [0, 20],    # Unknown_age_pct
-                [0, 100],   # 1960-1979_pct
-                [0, 100],   # 1919-1944_pct
-                [0, 100],   # Post 1999_pct
-                [30, 80],   # HDD
-                [0, 8],     # CDD
-                [3, 15],    # HDD_summer
-                [30, 60],   # HDD_winter
-                [1, 26000], # postcode_area
-                [0, 0.5],   # postcode_density
-                [5, 12],    # log_pc_area
-                [0, 1],     # ethnic_group_perc_White: English, Welsh, Scottish, Northern Irish or British
-                [0, 1],     # central_heating_perc_Mains gas only
-                [0, 1],     # household_siz_perc_perc_1 person in household
-                [1, 5],     # Average Household Size
-                [0, 2000],  # clean_res_premise_area_total
-                [0, 1000]   # all_res_base_floor_total
-            ]
+    
+    return new_problem
+
+# # Generate problems dynamically
+# problems = {}
+# for col_setting in settings_dict.keys():
+#     problems[col_setting] = generate_problem(col_setting)
+
+# # Special case for problem 44 with grouped version
+# if 44 in problems:
+#     problem_44_ungrouped = problems[44]
+#     problem_44_grouped = {
+#         'num_vars': problem_44_ungrouped['num_vars'],
+#         'groups': ['group_1', 'group_1', 'group_1', 'group_2', 'group_3', 'group_4', 'group_5', 'group_6', 'group_7', 'group_8', 'group_9', 
+#                    'group_10', 'group_11', 'group_12', 'group_13', 'group_12', 'group_12', 'group_14', 'group_15', 'group_14', 'group_15', 
+#                    'group_16', 'group_17', 'group_18', 'group_1', 'group_1'],
+#         'names': problem_44_ungrouped['names'],
+#         'bounds': problem_44_ungrouped['bounds']
+#     }
+#     problems[44] = {
+#         'ungrouped': problem_44_ungrouped,
+#         'grouped': problem_44_grouped
+#     }
+
+# Function to get problem definition
+def get_problem(col_setting, grouped=False):
+
+    problems = {}
+    
+    problems[col_setting] = generate_problem(col_setting)
+
+    # Special case for problem 44 with grouped version
+    if 44 in problems:
+        problem_44_ungrouped = problems[44]
+        problem_44_grouped = {
+            'num_vars': problem_44_ungrouped['num_vars'],
+            'groups': ['group_1', 'group_1', 'group_1', 'group_2', 'group_3', 'group_4', 'group_5', 'group_6', 'group_7', 'group_8', 'group_9', 
+                    'group_10', 'group_11', 'group_12', 'group_13', 'group_12', 'group_12', 'group_14', 'group_15', 'group_14', 'group_15', 
+                    'group_16', 'group_17', 'group_18', 'group_1', 'group_1'],
+            'names': problem_44_ungrouped['names'],
+            'bounds': problem_44_ungrouped['bounds']
         }
-    }
-}
+        problems[44] = {
+            'ungrouped': problem_44_ungrouped,
+            'grouped': problem_44_grouped
+        }
 
+    if col_setting == 44 and grouped:
+        return problems[44]['grouped']
+    return problems[col_setting]
 
-
-new_problem = {
-    'num_vars': len(feat_cols_3),
-    'names': feat_cols_3,
-    'bounds': [problem['bounds'][problem['names'].index(name)] for name in feat_cols_3]
-}
